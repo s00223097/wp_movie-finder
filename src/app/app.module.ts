5e61eb5de083f4f3680847ae6cd6adca6777d42d
Saniya Mathewkutty <S00223097@atu.ie>
2023-03-07 16:18:52 +0000

    http service reg

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,9 @@
+import { HttpClientModule } from '@angular/common/http'; // http service provider registration
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { OmdbApiService } from './services/omdb-api.service';
 
 @NgModule({
   declarations: [
@@ -9,8 +11,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule
+    HttpClientModule
   ],
-  providers: [],
+  providers: [OmdbApiService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
