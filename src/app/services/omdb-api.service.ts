b2d1210c274bf8626d771cb2ec14b80fb96c6229
Saniya Mathewkutty <S00223097@atu.ie>
2023-03-07 16:16:29 +0000

    added handleError method and configured class

--- src/app/services/omdb-api.service.ts
+++ src/app/services/omdb-api.service.ts
@@ -1,9 +1,36 @@
 import { Injectable } from '@angular/core';
 
+// Manually added imports
+
+import {HttpClient, HttpErrorResponse} from "@angular/common/http";
+import { Observable, throwError } from 'rxjs';
+import { catchError, tap } from 'rxjs';
+import { IOMDBResponse } from '../omdbresponse';
+
 @Injectable({
   providedIn: 'root'
 })
 export class OmdbApiService {
 
-  constructor() { }
+  private _siteURL="https://www.omdbapi.com/"
+  private _key="?apikey=8e949ef&t="
+  constructor(private _http:HttpClient) { }
+
+  // Add getMoviedata method
+  getMovieData(movieName:string):Observable<IOMDBResponse> 
+  {
+    return this._http.get<IOMDBResponse>(this._siteURL+this._key + movieName)
+    .pipe
+    (
+      tap(data => console.log('Moviedata/error' + JSON.stringify(data))
+      ),
+      catchError(this.handleError) // handle error method below vv
+    )
+  }
+  private handleError(err:HttpErrorResponse)
+  {
+    console.log('OmdbApiService:' + err.message);
+    // previous version return throwError("error : " + err.message);
+    return throwError(() => new Error("OmdbApiService:" + err.message))
+  }
 }
